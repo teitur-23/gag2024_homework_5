435778ac78255347ed2a888bee5f3467df46de10
teitur-23 <147160730+teitur-23@users.noreply.github.com>
2024-04-11 09:26:05 +0000

    yep

--- test.sql
+++ test.sql
@@ -1,9 +1,43 @@
-select 'Shows: SID --> S' as FD,
-case when count(*)=0 then 'MAY HOLD'
-else 'DOES NOT HOLD' end as Result
-from (
-    select B.BL, B.BNo
-    from Boats B
-    group by B.BL, B.BNo
-    having count(distinct B.Z) > 1
-) as X;
+select * from Projects
+limit 1;
+
+select * from Projects1
+natural join Projects2
+natural join Projects3
+natural join Projects4
+natural join Projects0
+limit 1;
+
+select * from Shows
+limit 1;
+
+select * from Shows1
+natural join Shows2
+natural join Shows0
+limit 1;
+
+select * from Rentals
+limit 1;
+
+select * from Rentals1
+natural join Rentals2
+natural join Rentals3
+natural join Rentals0
+limit 1;
+
+select * from Matches
+limit 1;
+
+select * from Matches1
+natural join Matches2
+natural join Matches3
+natural join Matches4
+natural join Matches0
+limit 1;
+
+select * from Boats
+limit 1;
+
+select * from Boats1
+natural join Boats0
+limit 1;
